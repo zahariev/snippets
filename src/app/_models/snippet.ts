e36f7e4eee34a971173d832746565ca3ebff7fb9
boyan zahariev <zaharievboyan@gmail.com>
2020-12-01 17:47:53 +0200

    add: snipped model

---
+++ src/app/_models/snippet.ts
@@ -0,0 +1,12 @@
+export class Snippet {
+  id: string;
+  title: string;
+  code: String;
+  tags: [];
+  likes: [];
+  countLikes: number;
+  createdBy: string;
+  modified: string;
+  private: boolean;
+  date: string;
+}
