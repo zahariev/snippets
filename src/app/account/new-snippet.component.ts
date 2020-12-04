f5bafb030c756e3b5d1d2c4d3e6c028a38f7d1d5
boyan zahariev <zaharievboyan@gmail.com>
2020-12-04 09:48:04 +0200

    fix: form validation

--- src/app/account/new-snippet.component.ts
+++ src/app/account/new-snippet.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DataService } from '../_services/data.service';
 
 @Component({
   selector: 'app-new-snippet',
@@ -8,48 +9,57 @@ import { Router } from '@angular/router';
 })
 export class NewSnippetComponent implements OnInit {
   form: FormGroup;
+  tags;
   submitted: boolean = false;
 
-  @Input('formGroup') userForm: FormGroup = new FormGroup({
-    title: new FormControl(''),
-    tags: new FormControl(''),
-    code: new FormControl(''),
-  });
+  // @Input('formGroup') userForm: FormGroup = new FormGroup({
+  //   title: new FormControl(''),
+  //   tags: new FormControl(''),
+  //   code: new FormControl(''),
+  // });
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private data: DataService
+  ) {
     this.form = this.formBuilder.group({
-      title: '',
-      tags: '',
+      title: ['', Validators.required],
+      tags: ['', Validators.required],
+      code: ['', Validators.required],
+      private: [''],
     });
   }
 
   ngOnInit(): void {}
 
-  onSubmit() {
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
     this.submitted = true;
 
-    // stop here if form is invalid
     if (this.form.invalid) {
       return;
     }
 
-    // this.loading = true;
-    // this.accountService
-    //   .register(this.form.value)
-    //   .pipe(first())
-    //   .subscribe({
-    //     next: () => {
-    //       this.alertService.success('Registration successful', {
-    //         keepAfterRouteChange: true,
-    //       });
-    //       this.router.navigate(['../login'], { relativeTo: this.route });
-    //     },
-    //     error: (err) => {
-    //       this.alertService.error(err.error);
-    //       this.loading = false;
-    //     },
-    //   });
-    // }
+    this.data.http
+      .post('/snippets/add', {
+        code: this.f.code.value,
+        title: this.f.title.value,
+        tags: this.f.tags.value,
+        private: this.f.private.value || false,
+      })
+      .subscribe({
+        next: (data) => {
+          if (data) {
+            // this.data.reload(data);
+            this.router.navigate(['/']);
+          }
+        },
+      });
+    return false;
   }
 
   cancel(ev) {
