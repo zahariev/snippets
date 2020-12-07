bc627da860ef4407b38e4fafc3b2e4e4c3d81862
boyan zahariev <zaharievboyan@gmail.com>
2020-12-07 11:12:53 +0200

    fix: isAdmin form value

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -25,6 +25,7 @@ export class RegisterComponent implements OnInit {
       lastName: ['', Validators.required],
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      isAdmin: [''],
     });
   }
 
