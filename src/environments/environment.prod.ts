54380bfe2ea638da06ac11f598db5f848c069991
boyan zahariev <zaharievboyan@gmail.com>
2020-12-07 12:21:45 +0200

    removed pusher keys from repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: '/api2',
-  PUSHER: {
-    APP_ID: '869250',
-    APP_KEY: '45c22c5576553782fef0',
-    APP_SECRET: '27783ad368527d5e243f',
-    APP_CLUSTER: 'eu',
-    APP_SECURE: 1,
-  },
 };
